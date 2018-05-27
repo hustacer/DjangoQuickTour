eab25560fc42643571aac0e737eede04eabf81ae
hustacer <xiaobinly@gmail.com>
2018-05-27 16:14:54 +0900

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from blog.models import *
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+    list_filter = ['timestamp']
+    search_fields = ['title']
+
+admin.site.register(Article, ArticleAdmin)
