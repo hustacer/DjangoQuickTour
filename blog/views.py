567f51bca8ecd617df5a6e6efb117e7c2a5892cc
hustacer <xiaobinly@gmail.com>
2018-05-27 16:52:14 +0900

    template

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def index(reqeust):
-    return HttpResponse('''<center><h1>For Fun</h1></center>
-            <span style='font-weight:bold;'>孔子曰:</span><span style='color:gray;'>学而时习之,不亦说乎</span>''')
+#    return HttpResponse('''<center><h1>For Fun</h1></center>
+#            <span style='font-weight:bold;'>孔子曰:</span><span style='color:gray;'>学而时习之,不亦说乎</span>''')
+    #loader的get_template函数加载页面模板
+    template = loader.get_template('blog/index.html')
+    #声明context字典
+    context = {
+        'authorName': 'LY',
+    }
+    return HttpResponse(template.render(context, reqeust))
